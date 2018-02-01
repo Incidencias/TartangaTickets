b63e1a577dd247e644827cf797f133510d903bde
Jon Zaballa <jzaballazarzosa@gmail.com>
2018-02-01 12:20:01 +0100

    Logic in progress

---
+++ TartangaTickets/TartangaTicketsApp/src/main/java/com/tartangatickets/utils/HibernateUtil.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tartangatickets.utils;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+/**
+ * Hibernate Utility class with a convenient method to get Session Factory
+ * object.
+ *
+ * @author ubuntu
+ */
+public class HibernateUtil {
+    private static final SessionFactory sessionFactory = buildSessionFactory();
+
+	private static SessionFactory buildSessionFactory() {
+		try {
+			
+                        Configuration config = new Configuration();
+                                
+                        // load hibernate.cfg.xml from different directory
+			SessionFactory sessionFactory = config.configure(
+					"/hibernate/hibernate.cfg.xml")
+					.buildSessionFactory();
+
+			return sessionFactory;
+
+		} catch (Throwable ex) {
+			// Make sure you log the exception, as it might be swallowed
+			System.err.println("Initial SessionFactory creation failed." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
+	public static void shutdown() {
+		// Close caches and connection pools
+		getSessionFactory().close();
+	}
+}
