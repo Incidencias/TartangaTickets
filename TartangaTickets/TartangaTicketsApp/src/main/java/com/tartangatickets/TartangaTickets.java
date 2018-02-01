0ba336d2ad596409a88b4ef3c37da982f355342c
Jon Zaballa <jzaballazarzosa@gmail.com>
2018-02-01 08:35:02 +0100

    [IMP] Project creation

---
+++ TartangaTickets/TartangaTicketsApp/src/main/java/com/tartangatickets/TartangaTickets.java
@@ -0,0 +1,33 @@
+package com.tartangatickets;
+
+import com.tartangatickets.views.PrimaryView;
+import com.tartangatickets.views.SecondaryView;
+import com.gluonhq.charm.glisten.application.MobileApplication;
+import com.gluonhq.charm.glisten.layout.layer.SidePopupView;
+import com.gluonhq.charm.glisten.visual.Swatch;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+
+
+public class TartangaTickets extends MobileApplication {
+
+    public static final String PRIMARY_VIEW = HOME_VIEW;
+    public static final String SECONDARY_VIEW = "Secondary View";
+    public static final String MENU_LAYER = "Side Menu";
+    
+    @Override
+    public void init() {
+        addViewFactory(PRIMARY_VIEW, () -> new PrimaryView(PRIMARY_VIEW).getView());
+        addViewFactory(SECONDARY_VIEW, () -> new SecondaryView(SECONDARY_VIEW).getView());
+        
+        addLayerFactory(MENU_LAYER, () -> new SidePopupView(new DrawerManager().getDrawer()));
+    }
+
+    @Override
+    public void postInit(Scene scene) {
+        Swatch.BLUE.assignTo(scene);
+
+        scene.getStylesheets().add(TartangaTickets.class.getResource("style.css").toExternalForm());
+
+    }
+}
