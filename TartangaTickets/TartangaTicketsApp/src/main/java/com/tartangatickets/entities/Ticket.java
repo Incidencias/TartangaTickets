901ab18db64d3d0337f00c5fc252067b410f0e06
Sergio López <catxser@gmail.com>
2018-02-02 08:27:51 +0100

    ticket fix3

--- TartangaTickets/TartangaTicketsApp/src/main/java/com/tartangatickets/entities/Ticket.java
+++ TartangaTickets/TartangaTicketsApp/src/main/java/com/tartangatickets/entities/Ticket.java
@@ -5,6 +5,7 @@
  */
 package com.tartangatickets.entities;
 
+import com.sun.istack.internal.NotNull;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.List;
@@ -51,10 +52,14 @@ public class Ticket implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+    @NotNull
     private Date createDate;
     private Date endDate;
+    @NotNull
     private String machineCode;
+    @NotNull
     private String department;
+    @NotNull
     private String location;
     @Enumerated(ORDINAL)
     private State state;
@@ -65,46 +70,13 @@ public class Ticket implements Serializable {
     @ManyToOne
     private Technician technician;
 
-    public List<Message> getMessages() {
-        return messages;
-    }
 
-    public void setMessages(List<Message> messages) {
-        this.messages = messages;
-    }
-
-    
     public Integer getId() {
         return id;
     }
 
     public void setId(Integer id) {
         this.id = id;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
-        if (!(object instanceof Ticket)) {
-            return false;
-        }
-        Ticket other = (Ticket) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "com.tartangatickets.entities.Ticket[ id=" + id + " ]";
     }
 
     public Date getCreateDate() {
@@ -162,7 +134,45 @@ public class Ticket implements Serializable {
     public void setUser(User user) {
         this.user = user;
     }
+
+    public List<Message> getMessages() {
+        return messages;
+    }
+
+    public void setMessages(List<Message> messages) {
+        this.messages = messages;
+    }
+
+    public Technician getTechnician() {
+        return technician;
+    }
+
+    public void setTechnician(Technician technician) {
+        this.technician = technician;
+    }
+
+
     
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Ticket)) {
+            return false;
+        }
+        Ticket other = (Ticket) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    } 
     
     
 }
